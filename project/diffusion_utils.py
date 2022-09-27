bdcc79de9e7649f7bbbf024cfa2b0e0c36469892
pieroit <piero.savastano@gmail.com>
2022-09-27 16:41:02 +0200

    inpainting notebook; new image loading utility;

--- project/diffusion_utils.py
+++ project/diffusion_utils.py
@@ -7,14 +7,21 @@ import torch
 import PIL
 from PIL import Image
 import numpy as np  
+
+def stable_diffusion_preprocess_image(image_path, mask_path=None, width=512, height=512):
     
-# image preprocessing
-def stable_diffusion_preprocess_image(image):
+    # force width and height to be multiple of 32
+    width, height = map(lambda x: x - x % 32, (width, height))
     
-    w, h = image.size
-    w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
-    image = image.resize((w, h), resample=PIL.Image.LANCZOS)
-    image = np.array(image).astype(np.float32) / 255.0
-    image = image[None].transpose(0, 3, 1, 2)
-    image = torch.from_numpy(image)
-    return 2.*image - 1.
+    image = Image.open(image_path).convert('RGB').resize((width, height))
+    
+    if mask_path:
+        mask = Image.open(mask_path).convert('RGB').resize((width, height))
+        side_by_side = Image.new('RGB', (width*2, height))
+        side_by_side.paste(image, (0, 0))
+        side_by_side.paste(mask, (width, 0))
+        return image, mask, side_by_side
+    
+    return image
+
+    
